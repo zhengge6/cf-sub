7dd8c95d6cbef640e33814b4431fd369ce8efa36
zhengge6 <78626189+zhengge6@users.noreply.github.com>
2026-09-12 09:15:01 +0800

    chore: only=reality SNI conesdlc.bristlecone.com

--- src/index.ts
+++ src/index.ts
@@ -191,7 +191,7 @@ proxies:
     flow: xtls-rprx-vision
     encryption: none
     packet-encoding: xudp
-    servername: www.sony.com
+    servername: conesdlc.bristlecone.com
     client-fingerprint: chrome
     reality-opts:
       public-key: jCmkxkAI6WpShwRODJvNnXb322wZR5OHc8tSZh_Xkx0
